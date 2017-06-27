2350cee818bd10999b16deafb821994cff707d8d
Kamil Ronewicz <galileox86@gmail.com>
2017-06-27 17:08:24 +0200

    Create ultra lightweight component

--- src/components/PrintCode.js
+++ src/components/PrintCode.js
@@ -2,28 +2,10 @@ import { h, Component } from 'preact'
 import linkState from 'linkstate'
 
 class PrintCode extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            text: null
-        }
-    }
-
-    onSubmit = (event) => {
-        event.preventDefault()
-        console.log(event)
-    }
-
-    render(props, { text = 'as'  }) {
+    render(props, { text = 'Default value' }) {
         return <div>
-            <form onSubmit={this.onSubmit}>
-                <input
-                    value={text}
-                    onInput={linkState(this, 'text')}
-                />
-                <pre>{JSON.stringify(this.state, null, 2)}</pre>
-            </form>
+            <input value={text} onInput={linkState(this, 'text')} />
+            <pre>{JSON.stringify(this.state, null, 2)}</pre>
         </div>
     }
 }
