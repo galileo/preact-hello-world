bdaa53911cceea91b3b8b9d97a138f1200040244
Kamil Ronewicz <galileox86@gmail.com>
2017-06-27 15:51:37 +0200

    Create user component and send values as props

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import { h } from 'preact'
+import User from './User'
+
+const Users = [
+    {name: 'Kamil Ronewicz', image: 'https://photos-6.dropbox.com/t/2/AAA8-vGyUOohs6IFZaj8hswX5h2_8ecx2RI32kbq6P83aQ/12/8347799/png/32x32/1/_/1/2/Heroized%20Kamil%202.png/EOmXmwYYq-ECIAcoBw/XUASvWanxV3rBpqVz5Vm4_eHe-7E5vXpFC-cGZCcyMo?size=2048x1536&size_mode=5'},
+    {name: 'Kamil Ronewicz (1)', image: 'https://photos-6.dropbox.com/t/2/AAA8-vGyUOohs6IFZaj8hswX5h2_8ecx2RI32kbq6P83aQ/12/8347799/png/32x32/1/_/1/2/Heroized%20Kamil%202.png/EOmXmwYYq-ECIAcoBw/XUASvWanxV3rBpqVz5Vm4_eHe-7E5vXpFC-cGZCcyMo?size=2048x1536&size_mode=5'},
+    {name: 'Kamil Ronewicz (2)', image: 'https://photos-6.dropbox.com/t/2/AAA8-vGyUOohs6IFZaj8hswX5h2_8ecx2RI32kbq6P83aQ/12/8347799/png/32x32/1/_/1/2/Heroized%20Kamil%202.png/EOmXmwYYq-ECIAcoBw/XUASvWanxV3rBpqVz5Vm4_eHe-7E5vXpFC-cGZCcyMo?size=2048x1536&size_mode=5'}
+]
 
 export function App(){
     return (
         <div class='app'>
-            <h1>Hello World</h1>
+            {Users.map(user => <User {...user} key={user.name} />)}
         </div>
     )
 }
