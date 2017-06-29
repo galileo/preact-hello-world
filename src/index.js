551408e9bc0fe38a1537a136b962fabe83ffd031
Kamil Ronewicz <galileox86@gmail.com>
2017-06-29 08:33:42 +0200

    Add redux DevTools

--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ import { h, render } from 'preact'
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import { Provider } from 'preact-redux'
 import thunk from 'redux-thunk'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import Home from './components/Home'
 import Profile from './components/Profile'
 import Error from './components/Error'
 import reducer from './reducers/userReducer'
 
-const store = createStore(
-  reducer,
-  { user: null, loading: true },
+const initialState = { user: null, loading: true }
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose
+
+const enhancer = composeEnhancers(
   applyMiddleware(thunk)
 )
+
+const store = createStore(reducer, initialState, enhancer)
 
 render(<div>
   <Provider store={store}>
